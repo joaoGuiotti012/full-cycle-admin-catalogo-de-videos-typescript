8c15f46696d029c389b523ec3e965cf5770e4b44
joaoGuiotti012 <gente.boa@hotmail.com>
2024-08-24 13:54:44 +0000

    Create struture to work with nest in same repo

--- jest.config.ts
+++ jest.config.ts
@@ -16,16 +16,16 @@ const config: Config = {
   // cacheDirectory: "/tmp/jest_rs",
 
   // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
+  // clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  // collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  // coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -33,7 +33,7 @@ const config: Config = {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  // coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -123,7 +123,7 @@ const config: Config = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: './src',
+  // rootDir: './src',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
@@ -137,9 +137,9 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: [
-    './shared/infra/testing/expect-helpers.ts'
-  ],
+  // setupFilesAfterEnv: [
+  //   './shared/infra/testing/expect-helpers.ts'
+  // ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -177,9 +177,9 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
-  },
+  // transform: {
+  //   '^.+\\.(t|j)sx?$': '@swc/jest',
+  // },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
@@ -198,6 +198,39 @@ const config: Config = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\..*spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': '@swc/jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '.interface.ts',
+    '-interface.ts',
+    'shared/testing',
+    'shared-module/testing',
+    'validator-rules.ts',
+    '-fixture.ts',
+    '.input.ts',
+    '.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['./core/shared/infra/testing/expect-helpers.ts'],
+  coverageProvider: 'v8',
+  collectCoverage: true,
+  clearMocks: true,
 };
 
 export default config;
---
+++ src/core/shared/infra/config.ts
@@ -0,0 +1,27 @@
+import { config as readEnv } from 'dotenv';
+import { join } from 'path';
+
+export class Config {
+  static env: any = null;
+
+  static db() {
+    Config.readEnv();
+
+    return {
+      dialect: 'sqlite' as any,
+      host: Config.env.DB_HOST,
+      logging: Config.env.DB_LOGGING === 'true',
+    }
+  }
+
+  // Le arquivo de configuração .env
+  // por padrão NODE_ENV pega o final .test
+  static readEnv() {
+    if (Config.env) {
+      return;
+    }
+    Config.env = readEnv({
+      path: join(__dirname, `../../../../envs/.env.${process.env.NODE_ENV}`),
+    }).parsed;
+  }
+}
